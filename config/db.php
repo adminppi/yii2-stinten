871ccf188db4c5b3953c1f79618df93aebb80f89
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2018-05-10 10:46:08 +0700

    ganti nama database

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=perus-stinten',
+    'dsn' => 'mysql:host=localhost;dbname=perpus-stinten',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
